59fa9c9aa158ad03d2032e37329b7e9182f14444
Victor Ayomide <vikkybliz01@gmail.com>
2021-10-10 00:45:22 +0100

    Fruit Chat App

---
+++ lib/chat_model.dart
@@ -0,0 +1,130 @@
+class FruitChatModel{
+  final String name;
+  final String message;
+  final String imgUrl;
+  FruitChatModel({required this.name, required this.message, required this.imgUrl});
+
+}
+
+List<FruitChatModel> chat = [
+  FruitChatModel(
+      name: 'Apple',
+      message: 'I am an apple',
+      imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Apple',
+    message: 'I am an apple',
+    imgUrl: 'https://images.unsplash.com/photo-1584306670957-acf935f5033c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  FruitChatModel(
+    name: 'Banana',
+    message: 'I am a banana',
+    imgUrl: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:hacktoberfest/chat_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,11 +14,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Calender App'),
+          title: Text('Fruit Chat'),
+          centerTitle: true,
         ),
-        body: TableCalendar(focusedDay: DateTime.now(), firstDay: DateTime.utc(2015), lastDay: DateTime.utc(2025)),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.message_outlined),
+        ),
+        body: ListView.builder(
+            itemCount: chat.length,
+            itemBuilder: (context, i) => ListTile(
+              leading: CircleAvatar(
+                maxRadius: 30,
+                backgroundColor: Colors.blueGrey,
+                backgroundImage: NetworkImage(chat[i].imgUrl),
+              ),
+              title: Text(chat[i].name, style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+              subtitle: Text(chat[i].message, style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+              ),),
+            )
+        ),
       ),
     );
   }
 }
-
